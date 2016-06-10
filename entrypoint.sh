10a00f3247dfa08930c10fe43a29d52a61655f04
蒼時弦也 <elct9620@frost.tw>
2016-06-10 19:17:09 +0800

    Add auto version via gitlab tag name and ref name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+VERSION=${CI_BUILD_TAG:-"${CI_BUILD_REF_NAME}"}
+
 # TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
 if [[ ! -z $SONAR_TOKEN ]]; then
-  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${SONAR_TOKEN}
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectVersion=${VERSION}
 else
   ${SONAR_SCANNER_HOME}/bin/sonar-scanner
 fi
