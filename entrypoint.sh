1fa986177cabdbb623537db20592b8fc41e895dc
蒼時弦也 <elct9620@frost.tw>
2016-06-10 18:35:41 +0800

    Fix variable define

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
 if [[ ! -z $SONAR_TOKEN ]]; then
-  ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1} -Dsonar.login ${SONAR_TOKEN}
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1} -Dsonar.login=${SONAR_TOKEN}
 else
   ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1}
 fi
