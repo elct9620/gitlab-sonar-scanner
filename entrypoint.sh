f7147458067fc7da5659b73317f85296726fd0d6
蒼時弦也 <elct9620@frost.tw>
2016-06-10 21:51:50 +0800

    Enable issue report on console

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION=${CI_BUILD_TAG:-"${CI_BUILD_REF_NAME}"}
 
-OPTS="-Dsonar.projectVersion=${VERSION} -Dsonar.gitlab.project_id=${CI_PROJECT_ID} -Dsonar.gitlab.commit_sha=${CI_BUILD_REF} -Dsonar.gitlab.ref_name=${CI_BUILD_REF_NAME}"
+OPTS="-Dsonar.projectVersion=${VERSION} -Dsonar.gitlab.project_id=${CI_PROJECT_ID} -Dsonar.gitlab.commit_sha=${CI_BUILD_REF} -Dsonar.gitlab.ref_name=${CI_BUILD_REF_NAME} -Dsonar.issuesReport.console.enable=true"
 
 # TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
