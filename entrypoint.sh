3022c49e1c78176ac60e4f970a780684aeab158e
蒼時弦也 <elct9620@frost.tw>
2016-06-10 18:33:58 +0800

    Inject Authoriztion token support

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+# TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
-${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1}
+if [[ ! -z $SONAR_TOKEN ]]; then
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1} -Dsonar.login ${SONAR_TOKEN}
+else
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${1}
+fi
