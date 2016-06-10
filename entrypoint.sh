7e38f7d87728ffae2a7850ff4de00fc884801475
蒼時弦也 <elct9620@frost.tw>
2016-06-10 19:23:46 +0800

    Add sonarqube gitlab report support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ set -e
 
 VERSION=${CI_BUILD_TAG:-"${CI_BUILD_REF_NAME}"}
 
+OPTS="-Dsonar.projectVersion=${VERSION} -Dsonar.gitlab.project_id=${CI_PROJECT_ID} -Dsonar.gitlab.commit_sha=${CI_BUILD_REF} -Dsonar.gitlab.ref_name=${CI_BUILD_REF_NAME}"
+
 # TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
 if [[ ! -z $SONAR_TOKEN ]]; then
-  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectVersion=${VERSION}
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${SONAR_TOKEN} ${OPTS}
 else
-  ${SONAR_SCANNER_HOME}/bin/sonar-scanner
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${OPTS}
 fi
